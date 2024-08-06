fb3ee7d059540d4ddf5dc0b935ebe311b5bd1473
hectorfigueroa <hectorfigueroa@eldiario.es>
2024-08-06 20:12:33 +0200

    refactor(tests): create a fixture for mock data

--- frontend/cypress/e2e/moviesList.cy.js
+++ frontend/cypress/e2e/moviesList.cy.js
@@ -1,39 +1,9 @@
 /* global cy */
-
 describe('App', () => {
   it('fetches movie data and displays it correctly', () => {
-    cy.intercept(
-      {
-        method: 'GET',
-        url: '/api/getMovies',
-      },
-      [{
-          id: 1,
-          title: 'Movie 1',
-          cover: 'https://example.com/movie1.jpg',
-          director: 'Director 1',
-          directorLink: 'https://example.com/director1',
-          stars: 6,
-          imdbLink: 'https://example.com/imdb1',
-        }, {
-          id: 2,
-          title: 'Movie 2',
-          cover: 'https://example.com/movie2.jpg',
-          director: 'Director 2',
-          directorLink: 'https://example.com/director2',
-          stars: 5.8,
-          imdbLink: 'https://example.com/imdb2',
-        }, {
-          id: 1,
-          title: 'Movie 3',
-          cover: 'https://example.com/movie3.jpg',
-          director: 'Director 1',
-          directorLink: 'https://example.com/director1',
-          stars: 8.6,
-          imdbLink: 'https://example.com/imdb3',
-        }]
+    cy.intercept('GET', '/api/getMovies', { fixture: 'movies.json' }
     ).as('getData') // assign an alias
-    
+
     cy.visit('/');
 
     cy.wait('@getData').then(() => {
